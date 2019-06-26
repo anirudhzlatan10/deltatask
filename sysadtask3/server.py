b451d287d31f39471cada92f8dc405a1db9edb1d
anirudhzlatan10 <49587268+anirudhzlatan10@users.noreply.github.com>
2019-06-26 09:21:52 +0530

    Update server.py
    bug fixes

--- sysadtask3/server.py
+++ sysadtask3/server.py
@@ -1,32 +1,34 @@
+
 import time, socket, sys
-print('Client Server...')
+print('Setup Server...')
 time.sleep(1)
 #Get the hostname, IP Address from socket and set Port
 soc = socket.socket()
-shost = socket.gethostname()
-ip = socket.gethostbyname(shost)
-#get information to connect with the server
-print(shost, '({})'.format(ip))
-server_host = input('Enter server\'s IP address:')
-name = input('Enter Client\'s name: ')
+host_name = socket.gethostname()
+ip = socket.gethostbyname(host_name)
 port = 1234
-print('Trying to connect to the server: {}, ({})'.format(server_host, port))
-time.sleep(1)
-soc.connect((server_host, port))
-print("Connected...\n")
-soc.send(name.encode())
-server_name = soc.recv(1024)
-server_name = server_name.decode()
-print('{} has joined...'.format(server_name))
-print('Enter [bye] to exit.')
+soc.bind((host_name, port))
+print(host_name, '({})'.format(ip))
+name = input('Enter name: ')
+soc.listen(1) #Try to locate using socket
+print('Waiting for incoming connections...')
+connection, addr = soc.accept()
+print("Received connection from ", addr[0], "(", addr[1], ")\n")
+print('Connection Established. Connected From: {}, ({})'.format(addr[0], addr[0]))
+#get a connection from client side
+client_name = connection.recv(1024)
+client_name = client_name.decode()
+print(client_name + ' has connected.')
+print('Press [bye] to leave the chat room')
+connection.send(name.encode())
 whileTrue:
-   message = soc.recv(1024)
-   message = message.decode()
-   print(server_name, ">", message)
-   message = input(str("Me > "))
-   if message == "[bye]":
-      message = "Leaving the Chat room"
-      soc.send(message.encode())
+   message = input('Me > ')
+   if message == '[bye]':
+      message = 'Good Night...'
+      connection.send(message.encode())
       print("\n")
       break
-   soc.send(message.encode())
+   connection.send(message.encode())
+   message = connection.recv(1024)
+   message = message.decode()
+   print(client_name, '>', message)
