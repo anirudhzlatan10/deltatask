e355fbd5fef88de2f33bfaabe60b9dacaa653a02
anirudhzlatan10 <49587268+anirudhzlatan10@users.noreply.github.com>
2019-07-01 10:04:01 +0530

    Update server.py

--- sysadtask3/server.py
+++ sysadtask3/server.py
@@ -1,34 +1,36 @@
+import time , socket , sys
+print('Setting-up Server...')
 
-import time, socket, sys
-print('Setup Server...')
-time.sleep(1)
-#Get the hostname, IP Address from socket and set Port
 soc = socket.socket()
 host_name = socket.gethostname()
 ip = socket.gethostbyname(host_name)
 port = 1234
-soc.bind((host_name, port))
-print(host_name, '({})'.format(ip))
-name = input('Enter name: ')
-soc.listen(1) #Try to locate using socket
-print('Waiting for incoming connections...')
-connection, addr = soc.accept()
-print("Received connection from ", addr[0], "(", addr[1], ")\n")
-print('Connection Established. Connected From: {}, ({})'.format(addr[0], addr[0]))
-#get a connection from client side
+soc.bind((host_name,port))
+print(host_name,'({})'.format(ip))
+name = input('Enter Name: ')
+soc.listen(1)
+
+print('Waiting for incoming connection...')
+connection.addr = soc.accept()
+print('Recieved Connection from'.addr[0].'('.addr[1].')\n')
+print('Connecton Established.\nConnected From : {}, ({})'.format(addr[0],addr[0]))
+
 client_name = connection.recv(1024)
 client_name = client_name.decode()
-print(client_name + ' has connected.')
-print('Press [bye] to leave the chat room')
+print(client_name + ' has connected ')
+print('Enter [bye] to leave the chatroom ')
 connection.send(name.encode())
-whileTrue:
-   message = input('Me > ')
-   if message == '[bye]':
-      message = 'Good Night...'
-      connection.send(message.encode())
-      print("\n")
-      break
-   connection.send(message.encode())
-   message = connection.recv(1024)
-   message = message.decode()
-   print(client_name, '>', message)
+
+while True:
+    message = input('Me > ')
+    if message=='bye':
+        message = 'Good bye.....'
+        connection.send(message.encode())
+        print("\n")
+        break
+    connection.send(message.encode())
+
+    message = connection.recv(1024)
+    message = message.decode()
+    print(client_name, ' > ', message)
+
